2a1664fb9873cfec3ec8111f18365954f211679e
Martijn Hanegraaf <martijnhanegraaf@gmail.com>
2020-11-12 14:19:55 +0100

    Ik wil geen petsen van Chiel

--- TheExpanse/Vector.cpp
+++ TheExpanse/Vector.cpp
@@ -1,1 +1,24 @@
 #include "Vector.h"
+
+#ifndef math
+#include <math.h>
+#endif
+
+//Constructors
+Vector::Vector() : x(0), y(0) {}
+Vector::Vector(float x, float y) : x(x), y(y) {}
+
+//Optellen
+Vector Vector::operator+ (const Vector& operand) {
+    return Vector(this->x + operand.x, this->y + operand.y);
+}
+
+//Aftrekken
+Vector Vector::operator- (const Vector& operand) {
+    return Vector(this->x - operand.x, this->y - operand.y);
+}
+
+//Vermenigvuldigen
+Vector Vector::operator* (const Vector& operand) {
+    return Vector(this->x * operand.x, this->y * operand.y);
+}
--- TheExpanse/Vector.h
+++ TheExpanse/Vector.h
@@ -1,6 +1,15 @@
 #pragma once
 class Vector
 {
+private:
+	int x;
+	int y;
 
+public:
+	Vector();
+	Vector(float x, float y);
+	Vector operator+(const Vector& operand);
+	Vector operator-(const Vector& operand);
+	Vector operator*(const Vector& operand);
 };
 
