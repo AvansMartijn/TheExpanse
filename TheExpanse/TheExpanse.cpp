1de9597884e26bfe9876b703c8eabf5aa9d63795
AvansChiel <cgmarts@avans.nl>
2020-12-18 14:41:34 +0100

    fixed typecasting errors, added five point star

--- TheExpanse/TheExpanse.cpp
+++ TheExpanse/TheExpanse.cpp
@@ -41,7 +41,8 @@ void drawStuff() {
     drawGraph();
 
     ExpanseHelper helper;
-    Matrix translationMatrix = helper.getTranslationMatrix(-5, -5);
+    //Matrix translationMatrix = helper.getTranslationMatrix(-5, -5);
+    Matrix translationMatrix = helper.getTranslationMatrix(0, 0);
 
     TwoDObject tdobj;
     for (int i = 0; i < tdobj.lines.size(); i++)
