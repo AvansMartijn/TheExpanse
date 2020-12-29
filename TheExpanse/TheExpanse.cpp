b1dd31c7f8593e55397b9595bdc169e1020b142b
AvansChiel <cgmarts@avans.nl>
2020-12-29 01:03:00 +0100

    added pulsating object(still needs improvement)

--- TheExpanse/TheExpanse.cpp
+++ TheExpanse/TheExpanse.cpp
@@ -16,204 +16,235 @@ ThreeDWindow window{ 1080, 720 };
 ExpanseHelper helper;
 TwoDObject tdobj;
 ThreeDObject threedobj;
+ThreeDObject target;
+int targetSize = 0;
+bool targetGrowing;
 
 
 void drawStuff() {
 
-    window.clear();
-    window.drawObject(threedobj);
-    window.renderPresent();
-    // Render the rect to the screen
+	window.clear();
+	window.drawObject(threedobj);
+	window.drawObject(target);
+	window.renderPresent();
+	// Render the rect to the screen
    /* window.clear();
-    window.renderSetViewport("topLeft");
-    drawGraph();
-    window.drawThreeDObject(threedobj);
-    window.renderSetViewport("topRight");
-    drawGraph();
-    window.drawThreeDObject(threedobj, "right");
-    window.renderSetViewport("bottomLeft");
-    drawGraph();
-    window.drawThreeDObject(threedobj, "top");
-   
-    window.renderPresent();*/
+	window.renderSetViewport("topLeft");
+	drawGraph();
+	window.drawThreeDObject(threedobj);
+	window.renderSetViewport("topRight");
+	drawGraph();
+	window.drawThreeDObject(threedobj, "right");
+	window.renderSetViewport("bottomLeft");
+	drawGraph();
+	window.drawThreeDObject(threedobj, "top");
+
+	window.renderPresent();*/
 
 }
 
+void pulseTarget() {
+	//TODO:: Make this error proof and dynamic and move to helper
+	if (targetGrowing) {
+		target = helper.scaleOnLocation(target, 1.01, 1.01, 1.01);
+		targetSize++;
+		if (targetSize >= 50) {
+			targetGrowing = false;
+		}
+	}
+	else {
+		target = helper.scaleOnLocation(target, 0.99, 0.99, 0.99);
+		targetSize--;
+		if (targetSize <= 0) {
+			targetGrowing = true;
+		}
+	}
+}
+
 void programLoop() {
-    
-    while (true)
-    {
-        // Get the next event
-        SDL_Event event;
-        if (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-            {
-                // Break out of the loop on quit
-                break;
-            }
-            if (event.type == SDL_KEYDOWN)
-            {
-                if (event.key.keysym.sym == SDLK_KP_PLUS)
-                {
-                    // A has been pressed
-                    threedobj = helper.scaleOnLocation(threedobj, 2, 2, 2);
+	unsigned int a = SDL_GetTicks();
+	unsigned int b = SDL_GetTicks();
+	double delta = 0;
 
-                }
-                if (event.key.keysym.sym == SDLK_KP_MINUS)
-                {
-                    // A has been pressed
-                    threedobj = helper.scaleOnLocation(threedobj, 0.5, 0.5, 0.5);
-                }
-                //if (event.key.keysym.sym == SDLK_UP)
-                //{
-                //    // A has been pressed
-                //    threedobj = helper.translateMatrix(threedobj, 0, 0.5, 0);
+	while (true)
+	{
+		a = SDL_GetTicks();
+		delta = a - b;
+		if (delta > 1000 / 60) {
+			b = a;
+			pulseTarget();
+			// Get the next event
+			SDL_Event event;
+			if (SDL_PollEvent(&event))
+			{
+				if (event.type == SDL_QUIT)
+				{
+					// Break out of the loop on quit
+					break;
+				}
+				if (event.type == SDL_KEYDOWN)
+				{
+					if (event.key.keysym.sym == SDLK_KP_PLUS)
+					{
+						// A has been pressed
+						threedobj = helper.scaleOnLocation(threedobj, 2, 2, 2);
 
-                //}
-                //if (event.key.keysym.sym == SDLK_DOWN)
-                //{
-                //    // A has been pressed
-                //    threedobj = helper.translateMatrix(threedobj, 0, -0.5, 0);
+					}
+					if (event.key.keysym.sym == SDLK_KP_MINUS)
+					{
+						// A has been pressed
+						threedobj = helper.scaleOnLocation(threedobj, 0.5, 0.5, 0.5);
+					}
+					//if (event.key.keysym.sym == SDLK_UP)
+					//{
+					//    // A has been pressed
+					//    threedobj = helper.translateMatrix(threedobj, 0, 0.5, 0);
 
-                //}
-                //if (event.key.keysym.sym == SDLK_LEFT)
-                //{
-                //    // A has been pressed
-                //    threedobj = helper.translateMatrix(threedobj, -0.5, 0, 0);
+					//}
+					//if (event.key.keysym.sym == SDLK_DOWN)
+					//{
+					//    // A has been pressed
+					//    threedobj = helper.translateMatrix(threedobj, 0, -0.5, 0);
 
-
-                //}
-                //if (event.key.keysym.sym == SDLK_RIGHT)
-                //{
-                //    // A has been pressed
-                //    threedobj = helper.translateMatrix(threedobj, 0.5, 0, 0);
-
-                //}
-                if (event.key.keysym.sym == SDLK_1)
-                {
-                    // A has been pressed
-                    threedobj = helper.translateMatrix(threedobj, 0, 0, 0.5);
-
-                }
-                if (event.key.keysym.sym == SDLK_2)
-                {
-                    // A has been pressed
-                    threedobj = helper.translateMatrix(threedobj, 0, 0, -0.5);
-
-                }
-                //PITCH bACK
-                if (event.key.keysym.sym == SDLK_s)
-                {
-                    // R has been pressed
-                    threedobj = helper.rotate(threedobj, 3, 'X');
-
-                }
-
-                //PITCH forward
-                if (event.key.keysym.sym == SDLK_w)
-                {
-                    // R has been pressed
-                    threedobj = helper.rotate(threedobj, -3, 'X');
-
-                }
-                //JAW RIGHT
-                if (event.key.keysym.sym == SDLK_d)
-                {
-                    // E has been pressed
-                    threedobj = helper.rotate(threedobj, 3, 'Y');
-
-                }
-
-                //JAW left
-                if (event.key.keysym.sym == SDLK_a)
-                {
-                    // E has been pressed
-                    threedobj = helper.rotate(threedobj, -3, 'Y');
-
-                }
-                //ROLL RIGHT
-                if (event.key.keysym.sym == SDLK_e)
-                {
-                    // T has been pressed
-                    threedobj = helper.rotate(threedobj, 3, 'Z');
-
-                }
-
-                //ROLL LEFT
-                if (event.key.keysym.sym == SDLK_q)
-                {
-                    // T has been pressed
-                    threedobj = helper.rotate(threedobj, -3, 'Z');
-
-                }
-                //CAMERA UP
-                if (event.key.keysym.sym == SDLK_PAGEUP)
-                {
-                    // T has been pressed
-                    window.camera.eye = window.camera.eye - window.camera.up;
+					//}
+					//if (event.key.keysym.sym == SDLK_LEFT)
+					//{
+					//    // A has been pressed
+					//    threedobj = helper.translateMatrix(threedobj, -0.5, 0, 0);
 
 
-                }
-                //CAMERA DOWN
-                if (event.key.keysym.sym == SDLK_PAGEDOWN)
-                {
-                    // T has been pressed
-                    window.camera.eye = window.camera.eye + window.camera.up;
+					//}
+					//if (event.key.keysym.sym == SDLK_RIGHT)
+					//{
+					//    // A has been pressed
+					//    threedobj = helper.translateMatrix(threedobj, 0.5, 0, 0);
+
+					//}
+					if (event.key.keysym.sym == SDLK_1)
+					{
+						// A has been pressed
+						threedobj = helper.translateMatrix(threedobj, 0, 0, 0.5);
+
+					}
+					if (event.key.keysym.sym == SDLK_2)
+					{
+						// A has been pressed
+						threedobj = helper.translateMatrix(threedobj, 0, 0, -0.5);
+
+					}
+					//PITCH bACK
+					if (event.key.keysym.sym == SDLK_s)
+					{
+						// R has been pressed
+						threedobj = helper.rotate(threedobj, 3, 'X');
+
+					}
+
+					//PITCH forward
+					if (event.key.keysym.sym == SDLK_w)
+					{
+						// R has been pressed
+						threedobj = helper.rotate(threedobj, -3, 'X');
+
+					}
+					//JAW RIGHT
+					if (event.key.keysym.sym == SDLK_d)
+					{
+						// E has been pressed
+						threedobj = helper.rotate(threedobj, 3, 'Y');
+
+					}
+
+					//JAW left
+					if (event.key.keysym.sym == SDLK_a)
+					{
+						// E has been pressed
+						threedobj = helper.rotate(threedobj, -3, 'Y');
+
+					}
+					//ROLL RIGHT
+					if (event.key.keysym.sym == SDLK_e)
+					{
+						// T has been pressed
+						threedobj = helper.rotate(threedobj, 3, 'Z');
+
+					}
+
+					//ROLL LEFT
+					if (event.key.keysym.sym == SDLK_q)
+					{
+						// T has been pressed
+						threedobj = helper.rotate(threedobj, -3, 'Z');
+
+					}
+					//CAMERA UP
+					if (event.key.keysym.sym == SDLK_PAGEUP)
+					{
+						// T has been pressed
+						window.camera.eye = window.camera.eye - window.camera.up;
 
 
-                }
-                //camera forward
-                if (event.key.keysym.sym == SDLK_UP)
-               {
-                   // A has been pressed
-                   //window.camera.eye = helper.getTranslationMatrix(0, 0, -0.5) * window.camera.eye;
-                    window.camera.eye = window.camera.eye - window.camera.direction;
-               }
-                //camera backward
-               if (event.key.keysym.sym == SDLK_DOWN)
-               {
-                   // A has been pressed
-                   window.camera.eye = window.camera.eye + window.camera.direction;
-               }
-               //camera left
-               if (event.key.keysym.sym == SDLK_LEFT)
-               {
-                   // A has been pressed
-                   //window.camera.eye = helper.getTranslationMatrix(-0.5, 0, 0) * window.camera.eye;
-                   window.camera.eye = window.camera.eye + window.camera.right;
+					}
+					//CAMERA DOWN
+					if (event.key.keysym.sym == SDLK_PAGEDOWN)
+					{
+						// T has been pressed
+						window.camera.eye = window.camera.eye + window.camera.up;
 
 
-               }
-               //camera right
-               if (event.key.keysym.sym == SDLK_RIGHT)
-               {
-                   // A has been pressed
-                   window.camera.eye = window.camera.eye - window.camera.right;
+					}
+					//camera forward
+					if (event.key.keysym.sym == SDLK_UP)
+					{
+						// A has been pressed
+						//window.camera.eye = helper.getTranslationMatrix(0, 0, -0.5) * window.camera.eye;
+						window.camera.eye = window.camera.eye - window.camera.direction;
+					}
+					//camera backward
+					if (event.key.keysym.sym == SDLK_DOWN)
+					{
+						// A has been pressed
+						window.camera.eye = window.camera.eye + window.camera.direction;
+					}
+					//camera left
+					if (event.key.keysym.sym == SDLK_LEFT)
+					{
+						// A has been pressed
+						//window.camera.eye = helper.getTranslationMatrix(-0.5, 0, 0) * window.camera.eye;
+						window.camera.eye = window.camera.eye + window.camera.right;
+
+
+					}
+					//camera right
+					if (event.key.keysym.sym == SDLK_RIGHT)
+					{
+						// A has been pressed
+						window.camera.eye = window.camera.eye - window.camera.right;
 
 
 
-               }
-               if (event.key.keysym.sym == SDLK_LSHIFT) {
-                   Vector v = threedobj.getDirectionVector();
-                   v = helper.normalize(v);
-                   threedobj = helper.translateMatrix(threedobj, v.x,  v.y,  v.z);
-               }
-            }
-         
-        }
-        drawStuff();
-    }
+					}
+					if (event.key.keysym.sym == SDLK_LSHIFT) {
+						Vector v = threedobj.getDirectionVector();
+						v = helper.normalize(v);
+						threedobj = helper.translateMatrix(threedobj, v.x, v.y, v.z);
+					}
+				}
+
+			}
+			drawStuff();
+		}
+	}
 }
 
 
 int main()
 {
-    //threedobj = helper.translateMatrix(threedobj,0, 0, -10);
-    
-   
-    programLoop();
-    return 0;
+	target = helper.translateMatrix(threedobj, 0, 0, -50);
+
+
+	programLoop();
+	return 0;
 }
 
 
